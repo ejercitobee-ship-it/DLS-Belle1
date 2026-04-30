0532755117aa8482b5134381da4102d7f3e4f9c6
ejercitobee-ship-it <ejercitobee@gmail.com>
2026-04-30 09:21:33 -0500

    Remove BrandBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import BrandBar from './components/BrandBar';
 import Collections from './components/Collections';
 import FeaturedProducts from './components/FeaturedProducts';
 import Bespoke from './components/Bespoke';
@@ -204,7 +203,6 @@ function AppInner() {
       ) : (
         <>
           <Hero />
-          <BrandBar />
           <Collections />
           <FeaturedProducts />
           <Bespoke />
